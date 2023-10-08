637d859fa1d563207ab0ff52337bc098352812e7
jyotiConeLabs <136764032+jyotiConeLabs@users.noreply.github.com>
2023-10-08 02:15:19 -0400

    Update generate_texture_patches.cpp

--- libs/tex/generate_texture_patches.cpp
+++ libs/tex/generate_texture_patches.cpp
@@ -39,8 +39,8 @@ T clamp(T const & v, T const & lo, T const & hi) {
 }
 
 void serializeToCLIA(const std::string& outputFileName,
-                     const std::map<unsigned long, unsigned int>& vertexToImageId,
-                     const std::map<unsigned int, std::string>& imageIdToName) {
+                     const std::map<unsigned long, int>& vertexToImageId,
+                     const std::map<int, std::string>& imageIdToName) {
     
     flatbuffers::FlatBufferBuilder builder;
 
@@ -129,7 +129,7 @@ struct TexturePatchCandidate {
 TexturePatchCandidate
 generate_candidate(int label, TextureView const & texture_view,
     std::vector<std::size_t> const & faces, mve::TriangleMesh::ConstPtr mesh,
-    Settings const & settings, std::map<unsigned long, unsigned int> &image_associations) {
+    Settings const & settings, std::map<unsigned long, int> &image_associations) {
     
     mve::ImageBase::Ptr view_image = texture_view.get_image();
     int min_x = view_image->width(), min_y = view_image->height();
@@ -147,7 +147,7 @@ generate_candidate(int label, TextureView const & texture_view,
             // std::cout << "Vertex: " << vertices[mesh_faces[faces[i] * 3 + j]] << std::endl;
             // std::cout << "Image Name: " << texture_view.image_file << std::endl;
 
-            image_associations[static_cast<unsigned long>(mesh_faces[faces[i] * 3 + j])] = static_cast<unsigned int>(label);
+            image_associations[static_cast<unsigned long>(mesh_faces[faces[i] * 3 + j])] = static_cast<int>(label);
             
             math::Vec3f vertex = vertices[mesh_faces[faces[i] * 3 + j]];
             math::Vec2f pixel = texture_view.get_pixel_coords(vertex);
@@ -538,8 +538,8 @@ generate_texture_patches(UniGraph const & graph, mve::TriangleMesh::ConstPtr mes
 
     std::size_t num_patches = 0;
 
-    std::map<unsigned long, unsigned int> image_associations; 
-    std::map<unsigned int, std::string> image_names;
+    std::map<unsigned long, int> image_associations; 
+    std::map<int, std::string> image_names;
 
     std::cout << "\tRunning... " << std::flush;
     #pragma omp parallel for schedule(dynamic)
