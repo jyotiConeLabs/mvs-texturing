e3bd194abdd485c8d3f68de68910ecd955eb7a9e
jyotiConeLabs <136764032+jyotiConeLabs@users.noreply.github.com>
2023-10-11 16:37:17 -0400

    Update generate_texture_patches.cpp

--- libs/tex/generate_texture_patches.cpp
+++ libs/tex/generate_texture_patches.cpp
@@ -541,7 +541,9 @@ generate_texture_patches(UniGraph const & graph, mve::TriangleMesh::ConstPtr mes
     std::vector<std::string> image_filenames;
     for (std::size_t i = 0; i < texture_views->size(); ++i) {
         TextureView * texture_view = &texture_views->at(i);
-        image_filenames.push_back(texture_view->image_file);
+        std::string path = texture_view->image_file;
+        std::string base_filename = path.substr(path.find_last_of("/\\") + 1);
+        image_filenames.push_back(base_filename);
     }
 
     #pragma omp declare reduction (merge : std::vector<std::pair<unsigned long, int>> : omp_out.insert(omp_out.end(), omp_in.begin(), omp_in.end()))
